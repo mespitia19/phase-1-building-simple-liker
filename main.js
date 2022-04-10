6cff806ffb6e7e3011a3f80f2dca85956b5b94ed
MaximoEspitia <max19espitia@gmail.com>
2022-04-10 13:19:17 -0500

    commit tree a9b3fef4a86f22f9d004c21942cf1e58be959cc1

--- main.js
+++ main.js
@@ -2,9 +2,46 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+//Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", () => {
+  const errorModal = document.querySelector("#modal")
+  errorModal.classList.add("hidden")
+  
+  //findLikes()
+  clickListener()
+})
 
+function hideError() {
+  errorModal.classList.add("hidden")
+
+}
+
+function findLikes(){
+  const likeArr = document.querySelectorAll(".like-glyph")
+
+  likeArr.forEach((singularLike) => {
+    singularLike.addEventListener("click", () => console.log("YOU FOUND ME"))
+  })
+}
+
+function clickListener(){
+  document.addEventListener('click', (event) => {
+    if(event.target.classList[0] === 'like-glyph') {
+      mimicServerCall()
+        .then(resp => {
+          event.target.classList.add("activated-heart")
+          //event.target.classList.remove("activated-heart")
+        })
+        .catch(error => {
+          setTimeout(() => {
+            hideError()
+            },3000)
+          })
+        }
+      })
+  }
+  
 
 
 //------------------------------------------------------------------------------
